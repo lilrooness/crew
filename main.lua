7882aed0aeeb51eacc97b604a47e6b12344d4e75
Joe Frangoudes <jfrangoudes@gmail.com>
2020-02-12 16:49:45 +0000

    map gen random walk

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 radio = require "radio"
+map = require "map"
 
 updateStep = 1000 / 60
 time = 0
@@ -22,51 +23,13 @@ end
 
 sentences = {}
 
-doors = {}
 
 people = {}
-
-rooms = {}
-
-room_types = {
-    "engines",
-    "cryo",
-    "bridge",
-    "weapons"
-}
 
 icons = {}
 
 function getIconQuad(x, y, w, h, image)
     icon_quad = love.graphics.newQuad(x, y, w, h, image:getWidth(), image:getHeight())
-end
-
-function createRoom(x, y, w, h)
-    if math.random(100) > 50 then
-        warning = "hazard"
-    end
-    return {
-        x = x,
-        y = y,
-        w = w,
-        h = h,
-        type = room_types[math.random(#room_types)],
-        showWarning = false,
-        warning = nil
-    }
-end
-
-function addDoor(room1, room2, isOpen)
-    local door = {
-        isOpen = isOpen,
-        room1 = room1,
-        room2 = room2
-    }
-
-    table.insert(doors, door)
-    index = table.getn(doors)
-
-    return index
 end
 
 function createPerson(name, x, y)
@@ -83,7 +46,7 @@ end
 
 function getConnectingDoor(room1, room2)
     print("room1 " .. room1 .. " room2 " .. room2)
-    for i, v in pairs(doors) do
+    for i, v in pairs(map.doors) do
         print(v.room1 .. " " .. v.room2)
         if (v.room1 == room1 and v.room2 == room2) or (v.room2 == room1 and v.room1 == room2) then
             print("returning door " .. i)
@@ -95,16 +58,16 @@ function getConnectingDoor(room1, room2)
 end
 
 function openDoor(person, door_id)
-    if person.state == nil and not doors[door_id].isOpen then
+    if person.state == nil and not map.doors[door_id].isOpen then
         local ticksLeft = 10
         speak(person.name, radio.door.calm_door_open(door_id))
         person.state = function(dt)
-            local done = ticksLeft < 1 or doors[door_id].isOpen -- door may have been opened by someone else
+            local done = ticksLeft < 1 or map.doors[door_id].isOpen -- door may have been opened by someone else
 
             ticksLeft = ticksLeft - 1
 
             if done then
-                doors[door_id].isOpen = true
+                map.doors[door_id].isOpen = true
                 speak(person.name, radio.door.calm_door_opened(door_id))
             end
 
@@ -114,16 +77,16 @@ function openDoor(person, door_id)
 end
 
 function closeDoor(person, door_id)
-    if person.state == nil and doors[door_id].isOpen then
+    if person.state == nil and map.doors[door_id].isOpen then
         local ticksLeft = 10
         speak(person.name, radio.door.calm_door_close(door_id))
         person.state = function(dt)
-            local done = ticksLeft < 1 or (not doors[door_id].isOpen) -- door may have been closed by someone else
+            local done = ticksLeft < 1 or (not map.doors[door_id].isOpen) -- door may have been closed by someone else
 
             ticksLeft = ticksLeft - 1
 
             if done then
-                doors[door_id].isOpen = false
+                map.doors[door_id].isOpen = false
                 speak(person.name, radio.door.calm_door_closed(door_id))
             end
 
@@ -133,7 +96,7 @@ function closeDoor(person, door_id)
 end
 
 function moveRoom(person, room_id)
-    if doors[getConnectingDoor(person.room, room_id)].isOpen then
+    if map.doors[getConnectingDoor(person.room, room_id)].isOpen then
         local ticksLeft = 3
         person.state = function(dt)
             local done = ticksLeft < 1
@@ -184,19 +147,19 @@ function love.draw()
 
     -- ROOMS
     love.graphics.setColor(1, 1, 1)
-    for i, v in pairs(rooms) do
+    for i, v in pairs(map.rooms) do
         love.graphics.rectangle("line", v.x, v.y, v.w, v.h)
         icon_x = (v.x + v.w / 3)
         icon_y = v.y + 3
         love.graphics.draw(icons[v.type], icon_x, icon_y, 0, 0.05, 0.05)
     end
 
-    -- DOORS
+    -- map.DOORS
     love.graphics.setColor(1, 0, 0)
-    for i, v in pairs(doors) do
+    for i, v in pairs(map.doors) do
         -- position the doors between the rooms midpoints
-        local x = ((rooms[v.room1].x + rooms[v.room1].w / 2) + (rooms[v.room2].x + rooms[v.room2].w / 2)) / 2
-        local y = ((rooms[v.room1].y + rooms[v.room1].h / 2) + (rooms[v.room2].y + rooms[v.room2].h / 2)) / 2
+        local x = ((map.rooms[v.room1].x + map.rooms[v.room1].w / 2) + (map.rooms[v.room2].x + map.rooms[v.room2].w / 2)) / 2
+        local y = ((map.rooms[v.room1].y + map.rooms[v.room1].h / 2) + (map.rooms[v.room2].y + map.rooms[v.room2].h / 2)) / 2
         local mode = "fill"
         if v.isOpen then
             mode = "line"
@@ -207,7 +170,7 @@ function love.draw()
     -- PEOPLE
     love.graphics.setColor(1, 1, 1)
     for i, v in pairs(people) do
-        love.graphics.rectangle("fill", rooms[people[i].room].x, rooms[people[i].room].y, 10, 10)
+        love.graphics.rectangle("fill", map.rooms[people[i].room].x, map.rooms[people[i].room].y, 10, 10)
     end
 
     -- UI
@@ -236,6 +199,8 @@ function love.keyreleased(key)
 end
 
 function love.load()
+    math.randomseed(os.time())
+
     for i = 1, 5 do
         table.insert(sentences, i, "this is a sentence" .. i)
     end
@@ -243,9 +208,8 @@ function love.load()
     table.insert(people, createPerson("Emily", 50, 50))
     table.insert(people, createPerson("David", 50, 50))
 
-    table.insert(rooms, createRoom(50, 50, 100, 100))
-    table.insert(rooms, createRoom(155, 50, 100, 100))
-    addDoor(1, 2, false)
+    -- create map
+    map.randomWalk(50)
 
     icons["bridge"] = love.graphics.newImage("img/radar-sweep-glow.png")
     icons["weapons"] = love.graphics.newImage("img/heavy-bullets-glow.png")
---
+++ map.lua
@@ -0,0 +1,90 @@
+local m = {
+	rooms = {},
+	doors = {},
+	room_types = {
+    	"engines",
+    	"cryo",
+    	"bridge",
+    	"weapons"
+	}
+}
+
+local n = {}
+
+
+function m.randomWalk(steps)
+
+	local xpos = 0
+	local ypos = 0
+	
+	local xstep = 105
+	local ystep = 105
+	
+	local firstRoom = n.createRoom(xpos, ypos, xstep-5, ystep-5)
+	table.insert(m.rooms, firstRoom)
+	local currentRoom = table.getn(m.rooms)
+
+	-- we will check this map to see if we have collided with ourselves
+	local taken = {
+		[xpos..":"..ypos] = currentRoom
+	}
+
+	-- we can go up and down each axis
+	local options = {-1, 1}
+
+	for i=0, steps do
+
+		local direction = options[math.random(#options)]
+
+		if math.random(2) == 1 then
+			xpos = xpos + (xstep * direction)
+		else
+			ypos = ypos + (ystep * direction)
+		end
+
+
+		if taken[xpos..""..ypos] == nil then
+			local room = n.createRoom(xpos, ypos, xstep-5, ystep-5)
+			table.insert(m.rooms, room)
+			newRoom = table.getn(m.rooms)
+			n.addDoor(currentRoom, newRoom, false)
+			taken[xpos..":"..ypos] = newRoom
+			currentRoom = newRoom
+
+		else
+			currentRoom = taken[xpos..""..ypos]
+		end
+	end
+
+end
+
+
+function n.createRoom(x, y, w, h)
+    if math.random(100) > 50 then
+        warning = "hazard"
+    end
+    return {
+        x = x,
+        y = y,
+        w = w,
+        h = h,
+        type = m.room_types[math.random(#m.room_types)],
+        showWarning = false,
+        warning = nil
+    }
+end
+
+function n.addDoor(room1, room2, isOpen)
+    local door = {
+        isOpen = isOpen,
+        room1 = room1,
+        room2 = room2
+    }
+
+    table.insert(m.doors, door)
+    index = table.getn(m.doors)
+
+    return index
+end
+
+return m
