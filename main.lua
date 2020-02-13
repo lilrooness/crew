654acbf3efe20fb56e114a847e042683b4604d42
Joe Frangoudes <jfrangoudes@gmail.com>
2020-02-13 18:22:09 +0000

    moved game state in to gameState table

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 radio = require "radio"
-map = require "map"
+local map = require "map"
 
 keys = {
   w = false,
@@ -9,7 +9,6 @@ keys = {
 }
 
 updateStep = 1000 / 60
-time = 0
 
 textbox = {
     x = 10,
@@ -18,25 +17,31 @@ textbox = {
     h = 5 * 15
 }
 
-local camera_x = 0
-local camera_y = 0
-
-lines_collection = {}
-
-for i, v in pairs(radio.door) do
-    table.insert(lines_collection, v)
-end
-
-for i, v in pairs(radio.chatter) do
-    table.insert(lines_collection, v)
-end
-
-sentences = {}
-
-
-people = {}
-
 icons = {}
+
+gameModes = {
+  MAP = 1,
+  PERSON_MENU = 2
+}
+
+items = {
+  plasmaCutter = "Plasma Cutter",
+  doorInterface = "Door Interface",
+  roomInterface = "Room Interface",
+  oxygenTank = "Oxygen Tank",
+  t1Scanner = "T1 Scanner",
+  t1PowerSource = "T1 Power Source"
+}
+
+local gameState = {
+  sentences = {},
+  gameMode = 1,
+  time = 0,
+  camera_x = 0,
+  camera_y = 0,
+  map = map,
+  people = {}
+}
 
 function getIconQuad(x, y, w, h, image)
   icon_quad = love.graphics.newQuad(x, y, w, h, image:getWidth(), image:getHeight())
@@ -44,6 +49,7 @@ end
 
 function createPerson(name, x, y)
   return {
+    items = {},
     name = name,
     stressLevel = 0,
     x = x,
@@ -55,7 +61,7 @@ function createPerson(name, x, y)
 end
 
 function getConnectingDoor(room1, room2)
-  for i, v in pairs(map.doors) do
+  for i, v in pairs(gameState.map.doors) do
     if (v.room1 == room1 and v.room2 == room2) or (v.room2 == room1 and v.room1 == room2) then
       return i
     end
@@ -65,17 +71,17 @@ function getConnectingDoor(room1, room2)
 end
 
 function openDoor(person, door_id)
-    if person.state == nil and not map.doors[door_id].isOpen then
+    if person.state == nil and not gameState.map.doors[door_id].isOpen then
         local ticksLeft = 10
         speak(person.name, radio.door.calm_door_open(door_id))
         person.state = function(dt)
-            local done = ticksLeft < 1 or map.doors[door_id].isOpen -- door may have been opened by someone else
+            local done = ticksLeft < 1 or gameState.map.doors[door_id].isOpen -- door may have been opened by someone else
 
             ticksLeft = ticksLeft - 1
 
             if done then
-                map.doors[door_id].isOpen = true
-                speak(person.name, radio.door.calm_door_opened(door_id))
+              gameState.map.doors[door_id].isOpen = true
+              speak(person.name, radio.door.calm_door_opened(door_id))
             end
 
             return done
@@ -84,16 +90,16 @@ function openDoor(person, door_id)
 end
 
 function closeDoor(person, door_id)
-    if person.state == nil and map.doors[door_id].isOpen then
+    if person.state == nil and gameState.map.doors[door_id].isOpen then
         local ticksLeft = 10
         speak(person.name, radio.door.calm_door_close(door_id))
         person.state = function(dt)
-            local done = ticksLeft < 1 or (not map.doors[door_id].isOpen) -- door may have been closed by someone else
+            local done = ticksLeft < 1 or (not gameState.map.doors[door_id].isOpen) -- door may have been closed by someone else
 
             ticksLeft = ticksLeft - 1
 
             if done then
-                map.doors[door_id].isOpen = false
+                gameState.map.doors[door_id].isOpen = false
                 speak(person.name, radio.door.calm_door_closed(door_id))
             end
 
@@ -103,7 +109,7 @@ function closeDoor(person, door_id)
 end
 
 function moveRoom(person, room_id)
-    if map.doors[getConnectingDoor(person.room, room_id)].isOpen then
+    if gameState.map.doors[getConnectingDoor(person.room, room_id)].isOpen then
         local ticksLeft = 3
         person.state = function(dt)
             local done = ticksLeft < 1
@@ -112,7 +118,7 @@ function moveRoom(person, room_id)
 
             if done then
               person.room = room_id
-              map.rooms[room_id].isVisible = true
+              gameState.map.rooms[room_id].isVisible = true
             end
 
             return done
@@ -131,25 +137,25 @@ function updatePerson(person, dt)
 end
 
 function shiftTextUp()
-    table.remove(sentences, 1)
+    table.remove(gameState.sentences, 1)
 end
 
 function speak(name, text)
-    if table.getn(sentences) > 4 then
-        table.remove(sentences, 1)
+    if table.getn(gameState.sentences) > 4 then
+        table.remove(gameState.sentences, 1)
     end
 
-    table.insert(sentences, name .. ": " .. text)
+    table.insert(gameState.sentences, name .. ": " .. text)
 end
 
 function love.draw()
 
     -- ROOMS
     love.graphics.setColor(1, 1, 1)
-    for i, v in pairs(map.rooms) do
-        love.graphics.rectangle("line", v.x-camera_x, v.y-camera_y, v.w, v.h)
-        icon_x = (v.x + v.w / 3) - camera_x
-        icon_y = v.y + 3 - camera_y
+    for i, v in pairs(gameState.map.rooms) do
+        love.graphics.rectangle("line", v.x-gameState.camera_x, v.y-gameState.camera_y, v.w, v.h)
+        icon_x = (v.x + v.w / 3) - gameState.camera_x
+        icon_y = v.y + 3 - gameState.camera_y
         if v.isVisible then
           love.graphics.draw(icons[v.type], icon_x, icon_y, 0, 0.05, 0.05)
           if v.warning then
@@ -160,28 +166,28 @@ function love.draw()
 
     -- map.DOORS
     love.graphics.setColor(1, 0, 0)
-    for i, v in pairs(map.doors) do
+    for i, v in pairs(gameState.map.doors) do
         -- position the doors between the rooms midpoints
-        local x = ((map.rooms[v.room1].x + map.rooms[v.room1].w / 2) + (map.rooms[v.room2].x + map.rooms[v.room2].w / 2)) / 2
-        local y = ((map.rooms[v.room1].y + map.rooms[v.room1].h / 2) + (map.rooms[v.room2].y + map.rooms[v.room2].h / 2)) / 2
+        local x = ((gameState.map.rooms[v.room1].x + gameState.map.rooms[v.room1].w / 2) + (gameState.map.rooms[v.room2].x + gameState.map.rooms[v.room2].w / 2)) / 2
+        local y = ((gameState.map.rooms[v.room1].y + gameState.map.rooms[v.room1].h / 2) + (gameState.map.rooms[v.room2].y + gameState.map.rooms[v.room2].h / 2)) / 2
         local mode = "fill"
         if v.isOpen then
             mode = "line"
         end
-        love.graphics.rectangle(mode, x-camera_x, y-camera_y, 10, 10)
+        love.graphics.rectangle(mode, x-gameState.camera_x, y-gameState.camera_y, 10, 10)
     end
 
     -- PEOPLE
     love.graphics.setColor(1, 1, 1)
-    for i, v in pairs(people) do
-      local x = map.rooms[people[i].room].x
-      local y = map.rooms[people[i].room].y
-        love.graphics.rectangle("fill", x-camera_x, y-camera_y, 10, 10)
+    for i, v in pairs(gameState.people) do
+      local x = gameState.map.rooms[gameState.people[i].room].x
+      local y = gameState.map.rooms[gameState.people[i].room].y
+        love.graphics.rectangle("fill", x-gameState.camera_x, y-gameState.camera_y, 10, 10)
     end
 
     -- UI
     love.graphics.setColor(1, 1, 1)
-    for i, v in pairs(people) do
+    for i, v in pairs(gameState.people) do
         love.graphics.draw(icons.person, 600, 50 * i, 0, 0.06, 0.06)
         love.graphics.print("[" .. i .. "] - " .. v.name, 630, 50 * i)
     end
@@ -192,48 +198,51 @@ function love.draw()
 
     -- CHAT
     love.graphics.setColor(1, 1, 1)
-    for i, v in pairs(sentences) do
+    for i, v in pairs(gameState.sentences) do
       love.graphics.print(v, textbox.x, textbox.y + ((i - 1) * 15))
     end
 end
 
 function love.update(dt)
+
   -- UPDATE CAMERA
-  time = time + dt
+  gameState.time = gameState.time + dt
 
-  local ydiff = 0
-  local xdiff = 0
-  if keys.w then
-    ydiff = ydiff - 1
+  if gameState.gameMode == gameModes.MAP then
+    local ydiff = 0
+    local xdiff = 0
+    if keys.w then
+      ydiff = ydiff - 1
+    end
+
+    if keys.s then
+      ydiff = ydiff + 1
+    end
+
+    if keys.a then
+      xdiff = xdiff - 1
+    end
+
+    if keys.d then
+      xdiff = xdiff + 1
+    end
+
+    local camSpeed = 5
+    gameState.camera_x = gameState.camera_x + xdiff * camSpeed
+    gameState.camera_y = gameState.camera_y + ydiff * camSpeed
   end
-
-  if keys.s then
-    ydiff = ydiff + 1
-  end
-
-  if keys.a then
-    xdiff = xdiff - 1
-  end
-
-  if keys.d then
-    xdiff = xdiff + 1
-  end
-
-  local camSpeed = 5
-  camera_x = camera_x + xdiff * camSpeed
-  camera_y = camera_y + ydiff * camSpeed
 
   -- UPDATE PEOPLE
-  for i, v in pairs(people) do
+  for i, v in pairs(gameState.people) do
     updatePerson(v, dt)
   end
 end
 
 function love.keyreleased(key)
     if key == "return" then
-        openDoor(people[1], 1)
+        openDoor(gameState.people[1], 1)
     elseif key == "space" then
-        moveRoom(people[1], 2)
+        moveRoom(gameState.people[1], 2)
     end
 
     keys[key] = false
@@ -247,11 +256,15 @@ function love.load()
     math.randomseed(os.time())
 
     for i = 1, 5 do
-        table.insert(sentences, i, "this is a sentence" .. i)
+        table.insert(gameState.sentences, i, "this is a sentence" .. i)
     end
 
-    table.insert(people, createPerson("Emily", 50, 50))
-    table.insert(people, createPerson("David", 50, 50))
+    table.insert(gameState.people, createPerson("Emily", 50, 50))
+    table.insert(gameState.people, createPerson("David", 50, 50))
+
+    table.insert(gameState.people[1].items, items.plasmaCutter)
+    table.insert(gameState.people[1].items, items.t1Scanner)
+    table.insert(gameState.people[2].items, items.t1PowerSource)
 
     -- create map
     map.randomWalk(50)
