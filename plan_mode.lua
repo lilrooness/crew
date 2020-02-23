d6eea9ed315a97a16f5e3b7830fb15951e9bb88a
JFrangoudes <lilrooness@users.noreply.github.com>
2020-02-23 17:22:40 +0000

    fixed scan render translation

--- plan_mode.lua
+++ plan_mode.lua
@@ -3,15 +3,21 @@ local m = {}
 local n = {}
 
 function m.renderPlanView(gameState)
-  local minX = 0
-  local minY = 0
-  local maxX = 0
-  local maxY = 0
+  local minX = nil
+  local minY = nil
+  local maxX = nil
+  local maxY = nil
 
   local screenWidth = love.graphics.getWidth()
   local screenHeight = love.graphics.getHeight()
 
   for i, v in pairs(gameState.map.rooms) do
+    if minX == nil then
+      minX = v.x
+      minY = v.y
+      maxX = v.x
+      maxY = v.y
+    end
     minX = math.min(v.x, minX)
     minY = math.min(v.y, minY)
     maxX = math.max(v.x, maxX)
@@ -21,12 +27,15 @@ function m.renderPlanView(gameState)
   local mapHeight = maxY - minY
   local mapWidth = maxX - minX
 
+  local midX = minX + (maxX / 2)
+  local midY = minY + (maxY / 2)
+
   local xScaleFactor = screenWidth / mapWidth
   local yScaleFactor = screenHeight / mapHeight
 
-  local scaleFactor = math.min(xScaleFactor, yScaleFactor) / 2
-  local xTrans = screenWidth / 2
-  local yTrans = screenHeight / 2
+  local scaleFactor = math.min(xScaleFactor, yScaleFactor)
+  local xTrans = minX * scaleFactor
+  local yTrans = minY * scaleFactor
 
   for i, v in pairs(gameState.map.rooms) do
     love.graphics.setColor(1, 1, 1)
@@ -36,16 +45,16 @@ function m.renderPlanView(gameState)
     if v.goalRoom then
       love.graphics.rectangle(
         "fill",
-        (v.x) * scaleFactor + xTrans,
-        (v.y) * scaleFactor + yTrans,
+        (v.x) * scaleFactor - xTrans,
+        (v.y) * scaleFactor - yTrans,
         v.w * scaleFactor,
         v.h * scaleFactor
       )
     else
       love.graphics.rectangle(
         "line",
-        (v.x) * scaleFactor + xTrans,
-        (v.y) * scaleFactor + yTrans,
+        (v.x) * scaleFactor - xTrans,
+        (v.y) * scaleFactor - yTrans,
         v.w * scaleFactor,
         v.h * scaleFactor
       )
